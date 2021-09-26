5d0068ca62e2072496095f6d3ed9b348ecf67f59
timothyo2 <89275303+timothyo2@users.noreply.github.com>
2021-09-26 14:15:28 -0400

    Add files via upload
    Files from August 20

---
+++ August20.R
@@ -0,0 +1,12 @@
+# Author: Pawar, Date: 08/20/21, purpose: calculate mode
+
+# variable 1
+variable1 <- c(19, 4, 5, 7, 29, 19, 29, 13, 25, 19)
+
+# calculate mode
+
+library(modeest)
+
+variable2 <- mlv(variable1, method = "mfv")
+print(variable2)
+# 19
