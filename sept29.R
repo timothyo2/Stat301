e425a73c962fe708a105f9eb31a30015ab0adc3e
timothyo2 <89275303+timothyo2@users.noreply.github.com>
2021-10-01 14:49:51 -0400

    Add files via upload
    Files from September 29

---
+++ sept29.R
@@ -0,0 +1,10 @@
+# Author: Pawar, Date: Sep 29 2021, Purpose: Calculate discrete probability for sample data
+
+db <- dbinom(4,size=12,prob=0.2)
+# 0.1328756
+
+pb <- pbinom(4,size=12,prob=0.2)
+# [1] 0.9274445
+
+print(db)
+print(pb)
